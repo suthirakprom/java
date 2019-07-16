fb36fdd7551da048232a0abc74acbadfee675613
Suthirak Prom <suthirakprom81@gmail.com>
2019-07-16 14:22:45 +0700

    Talk with the user

--- t.java
+++ t.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class t{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("My name is Prom suthirak");
+        System.out.print("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Hello, "+ name);
     }
 }
